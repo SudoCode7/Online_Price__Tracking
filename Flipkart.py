fd6d88131a212ea09eace796a1459db9512241de
SudoCode7 <63919871+SudoCode7@users.noreply.github.com>
2021-01-20 11:31:24 +0530

    First

--- Flipkart.py
+++ Flipkart.py
@@ -6,29 +6,25 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
-
+from mailjet_rest import Client
+import os
+api_key = '1234567890SDq8h38'  # Enter your api key here
+api_secret = 'Sahu2nuovselrh'  # Enter your api secret key here
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 URL = input("Enter the product URL= ")
-# print("\n")
 time_period = int(input("Enter the number of hours, you want the price to be checked in= "))
 print("\n")
-agent = input("Enter your brower agent, if you are not aware of that just GOOGLE 'My User Agent', while I wait............")
-print("\n")
-# ENTER YOUR USER AGENT BELOW, IF NOT SURE, SURF 'MY USER AGENT' ON GOOGLE
-HEADERS = {"User-Agent": {agent}}
 PRICE_VALUE_WANTED = float(input("What price do you except? "))  # ENTER THE PRICE VALUE YOU WANT TO BE CHECKED
 print("\n")
 EMAIL_ADDRESS = input("Enter your gmail address ")  # ENTER YOUR EMAIL ADDRESS
-print("Lastly enter your password, don't worry it's not going to be used anywhere and will only")
-Password = input("be used by the bot to login on your behalf(Don't believe me, check the code yourself :-))   ")
 print("\n")
 response = requests.get(URL)
 page = response.text
-soup = BeautifulSoup(page,'html.parser')
-name = soup.findAll('h1',class_= "yhB1nd")
+soup = BeautifulSoup(page, 'html.parser')
+name = soup.findAll('h1', class_="yhB1nd")
 title = name[0].get_text()
-price = soup.findAll('div',class_= '_30jeq3 _16Jk6d')
+price = soup.findAll('div', class_='_30jeq3 _16Jk6d')
 price1 = price[0].get_text()
-
 def trackPrices():
     price = float(Display_Price())
     if price > PRICE_VALUE_WANTED:
@@ -37,37 +33,49 @@ def trackPrices():
     else:
         print("Cheaper! Notifying...")
         sendEmail(price)
-    pass
-
+        pass
 
 def Display_Price():
-    print(title)
-    print(price1)
-    len1 = len(price1)
-    if len1 == 6:
-        price12 = price1[1:2] + price1[3:]
-    elif len1 == 7:
-        price12 = price1[1:3] + price1[4:]
-    else:
-        price12 = price1[1:]
-
-    return price12
-
+        print(title)
+        print(price1)
+        len1 = len(price1)
+        if len1 == 6:
+            price12 = price1[1:2] + price1[3:]
+        elif len1 == 7:
+            price12 = price1[1:3] + price1[4:]
+        else:
+            price12 = price1[1:]
+        return price12
 
 def sendEmail(price12):
     subject = "Flipkart Prize Dropped!!"
     price12 = str(price12)
     title1 = title.encode('utf-8')
-    mailtext = 'Subject:' + subject + '\n\n' + str(title1) + '- Rs' + price12 + '\n' + URL #RS CAN BE CHANGED TO YOUR CURRENCY
-    server = smtplib.SMTP(host='smtp.gmail.com', port=587)  # IF USING GOOGLE, DON'T TOUCH THIS FIELD OR ELSE
-    server.ehlo()  # SURF FOR REQUIRED EMAIL SERVICE AND IT'S PORT
-    server.starttls()
-    server.login(EMAIL_ADDRESS, Password)  # ENTER EMAIL-ID PASSWORD IN DOUBLE QUOTES GIVEN
-    server.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, mailtext)
-    print('Email Sent!\n\n')
+    mailtext = str(title1) + '- Rs' + price12 + '\n'  # RS CAN BE CHANGED TO YOUR CURRENCY
+    data = {
+        'Messages': [
+            {
+                "From": {
+                    "Email": EMAIL_ADDRESS,
+                    "Name": "Sudo" #Enter your name here
+                },
+                "To": [
+                    {
+                        "Email": EMAIL_ADDRESS,
+                        "Name": "Sudo" #Enter your name here
+                    }
+                ],
+                "Subject": subject,
+                "TextPart": mailtext,
+                "HTMLPart": URL,
+                "CustomID": "Flipkart_Price_Tracker"
+            }
+        ]
+    }
+    result = mailjet.send.create(data=data)
+    print (result.status_code)
+    print (result.json())
     pass
-
-
 if __name__ == "__main__":
     n = 0
     while n != 3:
